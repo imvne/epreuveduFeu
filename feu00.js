f6d3baa90c01a2f6f1c674c71796280bf52104da
imvne <imanebeaufils19@gmail.com>
2024-06-11 22:22:54 +0200

    échauffement

--- feu00.js
+++ feu00.js
@@ -3,18 +3,44 @@
 // Useful functions
 
 function buildRectangle(columns, lines){
-      
+	const rectangle = [];
+	
+	for (let i = 0 ; i < lines ; i++){
+		const lineBrick = "-";
+		const columnBrik = "|"
+		const corner = "o"
+		const whiteSpace = " ";
+
+		if (i === 0 || i === lines-1){
+			columns === 1 ? rectangle.push(corner) : 
+			rectangle.push(corner + lineBrick.repeat(columns - 2) + corner)
+		} else {
+			columns === 1 ? rectangle.push(columnBrik) : 
+			rectangle.push(columnBrik + whiteSpace.repeat(columns - 2) + columnBrik)
+		}
+		
+	}
+	return rectangle.join('\n')
 }
 
 // Error management
 
 function isValidArguments(arguments){
-      if (arguments.length > 1){
-            return true
+      if (arguments.length === 2){
+            return arguments
       }
-      return false
+	else {
+		return console.log("erreur : insérez deux arguments")
+	}
 }
 
+function isPositiveInteger(number){
+	if (number > 0 && Number.isInteger(Number(number))){ // n'inclus pas le zéro
+		return parseInt(number);
+	} else {
+		return console.log("erreur : n'insérez que des entiers positifs")
+	}
+}
 
 // Parsing
 
@@ -27,17 +53,25 @@ function getArguments(){
 // Solving
 
 function displayRectangle(){
-      const arguments = getArguments();
+      const numbers = isValidArguments(getArguments());
       
-      if (!isValidArguments(arguments)){
-            return "erreur : insérez au moins deux arguments"
-      }
-      else {
-            return arguments
-      }
+	if (!numbers){
+		return
+	}
+	
+	for (let i = 0 ; i < numbers.length ; i++){
+		if (!isPositiveInteger(numbers[i])){
+			return
+		} else {
+			numbers[i] = isPositiveInteger(numbers[i])
+		}
+	}
+	
+      return console.log(buildRectangle(numbers[0], numbers[1]))
+      
 }
 
 
 // Print
 
-console.log(displayRectangle())
+displayRectangle()
